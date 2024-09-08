27a2f5af5a6dfdf22f0b02f4bc125043241f2f60
Vladislav Khomyakov <khomy4kovvv@gmail.com>
2024-09-08 19:48:28 +0300

    [quadro] v2.0

--- quadro.c
+++ quadro.c
@@ -1,52 +1,104 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdlib.h>
 
-bool sign_of_diskr(double a, double b, double c){
-    double D = (b*b - 4*a*c);
-    if (D>0)
+struct coef
+{
+    double a;
+    double b;
+    double c;
+};
+
+struct roots
+{
+    double* root1;
+    double* root2;
+};
+
+struct coef quadro_input() {
+    struct coef coefs;
+    coefs.a = 0;
+    coefs.b = 0;
+    coefs.c = 0;
+    printf("Enter coefficient a: ");
+    while (scanf("%lf", &coefs.a) != 1) {
+        printf("Error input for a, try again: ");
+        while (getchar() != '\n'); // очистка буфера ввода
+    }
+
+    printf("Enter coefficient b: ");
+    while (scanf("%lf", &coefs.b) != 1) {
+        printf("Error input for b, try again: ");
+        while (getchar() != '\n'); // очистка буфера ввода
+    }
+
+    printf("Enter coefficient c: ");
+    while (scanf("%lf", &coefs.c) != 1) {
+        printf("Error input for c, try again: ");
+        while (getchar() != '\n'); // очистка буфера ввода
+    }
+
+    return coefs;
+}
+
+bool sign_of_diskr(double d){
+    if (d>0)
         return(true);
     else
         return(false);
 }
 
 double diskr(double a, double b, double c){
-    double D = (b*b - 4*a*c);
-    return (D);
+    return (b*b - 4*a*c);
+}
+
+struct roots quadro_output(double a, double b, double c) {
+    struct roots roots;
+    roots.root1 = NULL;
+    roots.root2 = NULL;
+
+    if (a == 0) {
+        if (b != 0) {
+            roots.root1 = (double*)malloc(sizeof(double));
+            *roots.root1 = -c / b;
+        }
+        return roots;
+    }
+
+    double d = diskr(a, b, c);
+    if (sign_of_diskr(d)) {
+        roots.root1 = (double*)malloc(sizeof(double));
+        roots.root2 = (double*)malloc(sizeof(double));
+        *roots.root1 = (-b + sqrt(d)) / (2 * a);
+        *roots.root2 = (-b - sqrt(d)) / (2 * a);
+    } else if (d == 0) {
+        roots.root1 = (double*)malloc(sizeof(double));
+        *roots.root1 = -b / (2 * a);
+    }
+
+    return roots;
 }
 
 int main(){
-    double a, b, c;
-    printf("vvedite a");
-    scanf("%lf", &a);
-    printf("vvedite b");
-    scanf("%lf", &b);
-    printf("vvedite c");
-    scanf("%lf", &c);
-
-    if (a==0){
-        printf("ne kvadratnoe");
+    struct coef coefs = quadro_input();
+    struct roots roots = quadro_output(coefs.a, coefs.b, coefs.c);
+    if (roots.root2 == NULL)
+    {
+        printf("roots: %lf", *roots.root1);
+        free(roots.root1);
         return 0;
     }
-
-    else{
-        bool s = sign_of_diskr(a, b, c);
-        double d = diskr(a, b, c);
-        if (s && d!=0){
-            double sol1 = ((-b + sqrt(d))/(2*a));
-            double sol2 = ((-b - sqrt(d))/(2*a));
-            printf("1st sol %f\n", sol1);
-            printf("2nd sol %f\n", sol2);
-            return 0;
-        }
-        else if (s && d==0){
-            double sol = ((-b)/(2*a));
-            printf("sol %f\n", sol);
-            return(0);
-        }
-        else{
-            printf("no sol in R\n");
-            return 0;
-        }
+    else if (roots.root1 == NULL && roots.root2 == NULL)
+    {
+        printf("no roots");
+        return 0;
+    }
+    else
+    {
+        printf("roots: %lf and %lf", *roots.root1, *roots.root2);       
+        free(roots.root1);
+        free(roots.root2);
+        return 0;
     }
 }
