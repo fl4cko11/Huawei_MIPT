742d2041bc2ba2213cab81f4f23f831244563039
Vladislav Khomyakov <khomy4kovvv@gmail.com>
2024-09-20 18:08:12 +0300

    quadro_with_h

---
+++ quadro_structs.h
@@ -0,0 +1,14 @@
+#ifndef QUADRO_STRUCTS_H
+#define QUADRO_STRUCTS_H
+
+struct roots{
+    double root1;
+    double root2;
+};
+
+struct coefs {
+    double a;
+    double b;
+    double c;
+};
+#endif
---
+++ solver.h
@@ -0,0 +1,39 @@
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <float.h>
+#include <assert.h>
+
+#include "quadro_structs.h"
+
+double diskr(double a, double b, double c) {
+    return b * b - 4 * a * c;
+}
+
+roots quadro_solution(coefs coefs){
+    roots roots = {NAN, NAN};
+    if (coefs.a == 0) {
+        if (coefs.b != 0) {
+            roots.root1 = -coefs.c / coefs.b;
+        }
+    } 
+    else {
+        double d = diskr(coefs.a, coefs.b, coefs.c);
+        if (d >= 0) {
+            double sqr_d = sqrt(d); //из-за точности дабла может в 0 уйти
+            if (sqr_d != 0){
+                roots.root1 = (-coefs.b + sqrt(d)) / (2 * coefs.a);
+                roots.root2 = (-coefs.b - sqrt(d)) / (2 * coefs.a);
+            } 
+            else {
+                roots.root1 = -coefs.b / (2 * coefs.a);
+            }
+        }
+    }
+    return roots;
+}
+
+#endif
