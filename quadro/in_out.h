f06ea69f08995586feddbe1a670d57aa4f16dda5
Vladislav Khomyakov <khomy4kovvv@gmail.com>
2024-09-22 20:11:08 +0300

    quadro_final 1.0

--- quadro/in_out.h
+++ quadro/in_out.h
@@ -9,43 +9,10 @@
 
 #include "quadro_structs.h"
 
-double coef_input(char coef_name) {
-    double coef = NAN; // init
-    int result = 0;
-    while (1) { // бесконечный цикл, который завершится при корректном вводе
-        printf("Enter coefficient %c: ", coef_name);
-        result = scanf("%lf", &coef);
-        if (result == 1) {
-            break; // корректный ввод, выход из цикла
-        } 
-        else {
-            printf("Not correct input, try again!\n");
-            while (getchar() != '\n'); // очистка буфера ввода
-        }
-    }
+double coef_input(char coef_name);
 
-    return coef;
-}
+coefs quadro_input();
 
-coefs quadro_input() {
-    coefs coefs = {NAN, NAN, NAN}; // NAN
-    coefs.a = coef_input('a');
-    coefs.b = coef_input('b');
-    coefs.c = coef_input('c');
-    return coefs;
-}
-
-void quadro_output(roots roots) {
-    if (1 - isnan(roots.root1)) {
-        printf("root1: %lf", roots.root1);
-        if (1 - isnan(roots.root2)) {
-            printf(", root2: %lf", roots.root2);
-        }
-        printf("\n");
-    } 
-    else {
-        printf("no roots\n");
-    }
-}
+void quadro_output(roots *roots);
 
 #endif
--- quadro/solver.h
+++ quadro/solver.h
@@ -1,7 +1,6 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
-#include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 #include <float.h>
@@ -9,36 +8,10 @@
 
 #include "quadro_structs.h"
 
-// declaration - объявление
-// definition - определение
+double diskr(coefs *coefs);
 
-double diskr(double a, double b, double c);
+bool is_zero(double d);
 
-double diskr(double a, double b, double c) { // definition
-    return b * b - 4 * a * c;
-}
-
-roots quadro_solution(coefs coefs){
-    roots roots = {NAN, NAN};
-    if (coefs.a == 0) {
-        if (coefs.b != 0) {
-            roots.root1 = -coefs.c / coefs.b;
-        }
-    } 
-    else {
-        double d = diskr(coefs.a, coefs.b, coefs.c);
-        if (d >= 0) {
-            double sqr_d = sqrt(d); //из-за точности дабла может в 0 уйти
-            if (sqr_d != 0){
-                roots.root1 = (-coefs.b + sqrt(d)) / (2 * coefs.a);
-                roots.root2 = (-coefs.b - sqrt(d)) / (2 * coefs.a);
-            } 
-            else {
-                roots.root1 = -coefs.b / (2 * coefs.a);
-            }
-        }
-    }
-    return roots;
-}
+roots quadro_solution(coefs *coefs);
 
 #endif
