70e48b5599abb5de781cad13c48696890cc1f644
Vladislav Khomyakov <khomy4kovvv@gmail.com>
2024-10-10 18:23:01 +0300

    [quadro] fixed after 2nd req

--- quadro/quadro_tests.cpp
+++ quadro/quadro_tests.cpp
@@ -8,26 +8,14 @@ bool is_equal(const double d1, const double d2) {
     else if (isnan(d1) && !isnan(d2) || !isnan (d1) && isnan(d2)) {
         return 0;
     }
-    else if (d1 == d2) {
+    else if (is_zero(d1-d2)) {
         return 1;
     }
     else return 0; 
 }
 
-bool is_roots_equal(roots res_roots, roots exp_roots) {
-    if (is_equal(res_roots.root1, exp_roots.root1)) {
-        if (is_equal(res_roots.root2, exp_roots.root2)) {
-            return true;
-        }
-        else return false;
-    }
-    else if (is_equal(res_roots.root1, exp_roots.root2)) {
-        if (is_equal(res_roots.root2, exp_roots.root1)) {
-            return true;
-        }
-        else return false;
-    }
-    else return false;
+bool is_equal(roots *res_roots, roots *exp_roots) {
+    return is_equal(res_roots->root1, exp_roots->root1) && is_equal(res_roots->root2, exp_roots->root2) || is_equal(res_roots->root1, exp_roots->root2) && is_equal(res_roots->root2, exp_roots->root1);
 }
 
 int main() {
@@ -44,17 +32,17 @@ int main() {
     roots roots = {};
     for (int i = 0; i < test_count; i++) {
         roots = quadro_solution(&tests[i].test_coefs);
-        if (is_roots_equal(roots, tests[i].exp_roots)) {
+        if (is_equal(&roots, &tests[i].exp_roots)) {
             printf("test %s good!\n", tests[i].testname);
             passed++;
         }
-        else printf("test %s went wrong!\n", tests[i].testname);
+        else printf("test number %d: %s went wrong!\n", passed + 1, tests[i].testname);
     }
 
     if (passed == test_count) {
-        printf("ALL TESTS PASSED!!!");
+        printf("ALL TESTS PASSED!!!\n");
     }
-    else printf("passed count: %d\n", passed);
+    else printf("passed count: %d from %d\n", passed, test_count);
 
     return 0;
 }
