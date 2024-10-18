6a54b0daa832af13fdca2b0e459758ad91151547
Vladislav Khomyakov <khomy4kovvv@gmail.com>
2024-10-18 10:05:42 +0300

    [SPU] add new funcs to header

--- SPU/spu_asm/spu_asm_funcs.h
+++ SPU/spu_asm/spu_asm_funcs.h
@@ -7,6 +7,10 @@ void lable_table_ctor(asm_lable_table *label_table, char *logname);
 
 void lable_table_dump (asm_lable_table *label_table);
 
+int first_spec_symb_in_str(char *str);
+
+int label_to_ip(char *temp_s, asm_lable_table *label_table);
+
 int command_cmp_and_writer(char cmd[], FILE *program_code, asm_lable_table *label_table, int number_of_str);
 
 #endif
